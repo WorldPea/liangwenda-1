f6556eeab88c12cac69921dffb1e7175ba96a22d
Administrator <1107591183@qq.com>
2018-01-24 23:09:18 +0800

    test1

---
+++ js-exercise/js/day1-122-1.1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+function B(){
+    alert("B函数");
+}
+B();
---
+++ js-exercise/js/day1-22-1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2018/1/22.
+ */
+function A() {
+    alert("A函数");
+}
+A();
